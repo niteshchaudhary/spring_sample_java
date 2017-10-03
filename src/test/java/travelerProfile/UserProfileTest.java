0f6916eec5c4c9e749a8fa8dc1e227b9b5e5f9ed
nchaudhary <nchaudhary@homeaway.com>
2017-10-03 16:17:33 -0500

    new commit

--- src/test/java/travelerProfile/UserProfileTest.java
+++ src/test/java/travelerProfile/UserProfileTest.java
@@ -1,12 +1,13 @@
 package travelerProfile;
 
 
-import config.UserContextConfiguration;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import config.UserContextConfiguration;
 import utils.UserContext;
 
 @ContextConfiguration(classes = {UserContextConfiguration.class})
@@ -23,6 +24,7 @@ public class UserProfileTest {
     public void user_profile() throws Throwable {
         user = userContext.getUser();
         System.out.println(user);
+        System.out.println("testing");
 
 
     }
